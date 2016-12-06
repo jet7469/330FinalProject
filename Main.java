608da91c118a50c3877ec2cad30e1f07f26641c5
sricci20 <ricci.steven@ymail.com>
2016-12-06 16:30:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
-
+/**Java Database Connectivity Final Project
+*Course Title: Java Data Connectivity and Access 
+*Course Number: ISTE-330
+*Instructor: Professor Floeser
+*@author Jenna Tillotson, Louis Trapani, Rosalee Hacker, Steven Ricci
+*@version 1.0, 12/7/2016
+*
+*Description: Main Class
+*"This class contains our main method for
+*running our projects program.
+*Simply a new Login view object is established which is where
+*our users will be brought to the login screen"
+*/
 //main class
 public class Main {
    
+   /**This method established a Login View object which
+   *pull up our our GUI for login
+   *@param args command line arguments ignored
+   */
    public static void main (String [] args) {
       new LogInView();
       //new View();
