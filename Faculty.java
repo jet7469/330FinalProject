f98a5abca263052ae54ba32188b217a3a451c8ac
sricci20 <ricci.steven@ymail.com>
2016-12-06 17:44:25 -0500

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -111,10 +111,16 @@ public class Faculty extends DLUser {
    }   
    
    
-   /**fetch metod use the Database class getMethod class
-   *to retrieve values for particular attributes 
-   *and run and update
-   *@return Attribute Data Boolean
+    /** 
+   *  fetch method calls getData with a mySQL SELECT
+   * statement to retrieve an entry from the DB.
+   * Once entry is retrieved it will check if there
+   * is data available. If so, it will set the object
+   * attributes with that information and return true.
+   * If data is not avialable it will return false
+   *
+   * @param mysql - the database to use
+   * @return a boolean
    */
    public boolean fetch() throws DLException {
       ArrayList<ArrayList<String>> result;
